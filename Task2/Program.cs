fb14d20ddd036feb9bdae929ad116971499d318e
AnastasiaRekalo <global-rekalonastya@gmail.com>
2023-10-04 18:07:23 +0300

    Решение задач

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SummaEll(int M, int N)
+{
+    if(M==N) return N;
+    else return M + SummaEll(M+1, N);;
+}
+
+int res = SummaEll(4, 8);
+Console.Write(res);
---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29 
+
+
+  int Akkerman(int m, int n)
+{
+  if (m == 0)
+    return n + 1;
+  else
+    if ((m != 0) && (n == 0))
+      return Akkerman(m - 1, 1);
+    else
+      return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+
+
+Console.WriteLine($"A({2},{3}) = {Akkerman(2, 3)}");
